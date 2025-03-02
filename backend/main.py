640079e7450e4e9cb05fb56d1697c4f9a05e2151
KamilWojas <wojaskamil@yahoo.pl>
2025-03-02 21:23:46 +0100

    Merge remote-tracking branch 'origin/main'

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,11 @@ app = FastAPI()
 # Wczytanie modelu NLP do analizy fake newsów
 nlp_model = pipeline("text-classification", model="facebook/bart-large-mnli")
 
-# Model wejściowy dla anlizy tekstów
-class TextAnalystRequest(BaseModel):
+
+# Model wejściowy dla analizy tekstu
+class TextAnalysisRequest(BaseModel):
     text: str
+
 
 # Model odpowiedzi API
 class AnalysisResponse(BaseModel):
@@ -18,9 +20,28 @@ class AnalysisResponse(BaseModel):
     source_reliability: float
     classification: str
 
-@app.post("/analyst_text", response_model=AnalysisResponse)
-def analyze_text(request: TextAnalystRequest):
+
+@app.post("/analyze_text", response_model=AnalysisResponse)
+def analyze_text(request: TextAnalysisRequest):
     """
-     Endpoint do analizy tekstu pod kątem fake newsów.
+    Endpoint do analizy tekstu pod kątem fake newsów.
     Zwraca ocenę prawdopodobieństwa fałszywej informacji oraz ocenę wiarygodności źródła.
-        """
+    """
+    # Analiza NLP
+    result = nlp_model(request.text)
+    classification_label = result[0]['label']
+    fake_news_score = result[0]['score'] if classification_label == "FAKE" else (1 - result[0]['score'])
+    source_reliability = 1 - fake_news_score
+
+    return AnalysisResponse(
+        text=request.text,
+        fake_news_score=fake_news_score,
+        source_reliability=source_reliability,
+        classification=classification_label
+    )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
